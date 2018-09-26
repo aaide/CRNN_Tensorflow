56d64f3db1647d4f6e750be2254dc7799d605710
Miguel de Benito Delgado <debenito@unternehmertum.de>
2018-09-26 16:25:29 +0200

    Add mongo worker

---
+++ tools/mongo-worker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# We need to import all modules required when running the jobs in the mongo db
+import sys
+import os
+import pickle
+from typing import Tuple, Union
+
+import tensorflow as tf
+import os.path as ops
+import time
+import numpy as np
+import argparse
+from easydict import EasyDict
+from hyperopt import hp, fmin, Trials, STATUS_FAIL, STATUS_OK, tpe, rand
+from hyperopt.mongoexp import MongoTrials, main_worker
+
+from crnn_model import crnn_model
+from local_utils import data_utils, log_utils
+from local_utils.log_utils import compute_accuracy
+from local_utils.config_utils import load_config
+
+logger = log_utils.init_logger()
+
+sys.exit(main_worker())
